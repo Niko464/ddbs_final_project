04015b2c6d49bcae05f75b4388f2f0deab8c25d0
De Smet Nikolaj <nikolaj.de-smet@epitech.eu>
2022-12-18 01:12:30 +0100

    Updating uploading script

--- src/uploadAllArticles.py
+++ src/uploadAllArticles.py
@@ -3,17 +3,20 @@ import os
 from pathlib import Path
 import time
 
+
+
 def uploadEntireArticle(hdfsclient, path, articleId):
-    for root, dirs, files in os.walk(f"{path}"):
-        for file in files:
-            local_path = os.path.join(root, file)
-            hdfs_path = f"/data/articles/{articleId}/" + file
-            print(f"Uploading {local_path} to {hdfs_path}")
-            hdfsclient.upload(
-                hdfs_path=hdfs_path,
-                local_path=local_path,
-                overwrite=True,
-            )
+    # for root, dirs, files in os.walk(f"{path}"):
+    #     for file in files:
+    local_path = path#os.path.join(root, file)
+    hdfs_path = f"/data/articles/{articleId}/"
+    print(f"Uploading {local_path} to {hdfs_path}")
+    hdfsclient.upload(
+        hdfs_path=hdfs_path,
+        local_path=local_path,
+        overwrite=True,
+        cleanup=True,
+    )
 
 
 
@@ -23,11 +26,21 @@ def main():
     ARTICLES_DIR = f"{DATA_FOLDER}/articles/"
 
     start = time.time()
-    files_and_directories = os.listdir(ARTICLES_DIR)
-    for item in files_and_directories:
-        path = os.path.join(ARTICLES_DIR, item)
-        if os.path.isdir(path):
-            uploadEntireArticle(hdfsclient, path, item)
+    # files_and_directories = os.listdir(ARTICLES_DIR)
+    print("Starting...")
+    # uploadEntireArticle(hdfsclient, "../data/articles/article1", "article1")
+    hdfsclient.upload(
+        hdfs_path="/data/articles/",
+        local_path=ARTICLES_DIR,
+        overwrite=True,
+        n_threads=256,
+        cleanup=True,
+    )
+
+    # for item in files_and_directories:
+    #     path = os.path.join(ARTICLES_DIR, item)
+    #     if os.path.isdir(path):
+    #         uploadEntireArticle(hdfsclient, path, item)
     end = time.time()
     print(f"Time taken to upload: {round(end - start, 2)}s")
 
